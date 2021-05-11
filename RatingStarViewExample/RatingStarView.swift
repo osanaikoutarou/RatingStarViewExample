9637cf3d9d78c88bb6aa849737989e307ad5a5da
osanaikoutarou <osanaikoutarou@gmail.cpm>
2021-05-11 11:35:13 +0900

    first commit

---
+++ RatingStarViewExample/RatingStarView.swift
@@ -0,0 +1,20 @@
+//
+//  RatingStarView.swift
+//  RatingStarViewExample
+//
+//  Created by 長内幸太郎 on 2021/05/10.
+//
+
+import UIKit
+
+class RatingStarView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
